200c9c8f54e5209029989f35c03833d960d90300
markdaniel1982 <>
2023-06-14 07:12:17 +0000

    started project, added profile app

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-@h$veb_%l3@u(s*$se2)(6&434n-u!k7#ijzyk$16##61k6u_&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'profiles',
 ]
 
 MIDDLEWARE = [
