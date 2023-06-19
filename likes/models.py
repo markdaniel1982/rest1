80fd76f690a7feeffd99ccf335e53579abe7fed4
markdaniel1982 <>
2023-06-19 06:54:39 +0000

    create LikeList and LikeDetail views

--- likes/models.py
+++ likes/models.py
@@ -4,8 +4,15 @@ from posts.models import Post
 
 
 class Like(models.Model):
+    """
+    Like model, related to 'owner' and 'post'.
+    'owner' is a User instance and 'post' is a Post instance.
+    'unique_together' makes sure a user can't like the same post twice.
+    """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='likes', on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
